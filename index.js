3666804f22f447660c886c5d1f488af8e56424d2
nhamilton1 <nickh3592@gmail.com>
2021-10-01 09:27:55 -0400

    moved index.js code to the top

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+require('dotenv').config()
+const server = require('./api/server')
+
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
+    console.log(`\n* Server Running on http://localhost:${port} *\n`)
+})
+
 /*
 play this: https://www.youtube.com/watch?v=d-diB65scQU
 
@@ -12,11 +21,4 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
-require('dotenv').config()
-const server = require('./api/server')
 
-const port = process.env.PORT || 5000
-
-server.listen(port, () => {
-    console.log(`\n* Server Running on http://localhost:${port} *\n`)
-})
