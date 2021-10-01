9fba1a9f146742d0c50f6b045979ac84ef6efab0
nhamilton1 <nickh3592@gmail.com>
2021-09-30 20:40:44 -0400

    actions put and delete completed

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -25,4 +25,20 @@ router.post('/', validateAction, (req, res, next) => {
         .catch(next)
 })
 
+router.put('/:id', validateActionId, validateAction, (req, res, next) => {
+    Actions.update(req.params.id, req.body)
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(next)
+})
+
+router.delete('/:id', validateActionId, (req, res, next) => {
+    Actions.remove(req.params.id)
+        .then(() => {
+            res.status(200).json(req.actions)
+        })
+        .catch(next)
+})
+
 module.exports = router
